2634e5986cd0fba6e93c52998f0a8b5006822223
Vinícius Oliveira <vinicius_rr1@hotmail.com>
2021-05-09 12:07:11 -0300

    implemantando so services de cliente e produto

---
+++ RestApiModelDDD.Domain.Services/ServiceProduto.cs
@@ -0,0 +1,17 @@
+﻿using RestApiModelDDD.Domain.Core.Interfaces.Repositorys;
+using RestApiModelDDD.Domain.Core.Interfaces.Services;
+using RestApiModelDDD.Domain.Entitys;
+
+namespace RestApiModelDDD.Domain.Services
+{
+    public class ServiceProduto : ServiceBase<Produto>, iServiceProduto
+    {
+        private readonly IRepositoryProduto repositoryProduto;
+
+        public ServiceProduto(IRepositoryProduto repositoryProduto)
+            :base(repositoryProduto)
+        {
+            this.repositoryProduto = repositoryProduto;
+        }
+    }
+}
