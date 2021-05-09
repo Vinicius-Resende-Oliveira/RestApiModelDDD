4cb789489788c97ca186958cfd97191ebf7283b9
Vinícius Oliveira <vinicius_rr1@hotmail.com>
2021-05-09 12:25:19 -0300

    Criando interfaces da Application

---
+++ RestApiModelDDD.Application/Interfaces/IApplicationSeriveProduto.cs
@@ -0,0 +1,14 @@
+﻿using RestApiModelDDD.Application.Dtos;
+using System.Collections.Generic;
+
+namespace RestApiModelDDD.Application.Interfaces
+{
+    public interface IApplicationSeriveProduto
+    {
+        void Add(ProdutoDto produtoDto);
+        void Upadte(ProdutoDto produtoDto);
+        void Remove(ProdutoDto produtoDto);
+        IEnumerable<ProdutoDto> GetAll();
+        ProdutoDto GetById(int id);
+    }
+}
