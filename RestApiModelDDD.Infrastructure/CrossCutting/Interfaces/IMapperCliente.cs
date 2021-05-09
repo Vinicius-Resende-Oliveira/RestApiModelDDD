6af3c6a951295b40c997a95564a7b9d225e0683a
Vinícius Oliveira <vinicius_rr1@hotmail.com>
2021-05-09 12:35:15 -0300

    Criando Interfaces de mapeamento de Entidades e Dtos

---
+++ RestApiModelDDD.Infrastructure/CrossCutting/Interfaces/IMapperCliente.cs
@@ -0,0 +1,13 @@
+﻿using RestApiModelDDD.Application.Dtos;
+using RestApiModelDDD.Domain.Entitys;
+using System.Collections.Generic;
+
+namespace RestApiModelDDD.Infrastructure.CrossCutting.Interfaces
+{
+    public interface IMapperCliente
+    {
+        Cliente MapperDtoToEntity(ClienteDto clienteDto);
+        IEnumerable<ClienteDto> MapperListClientesDto(IEnumerable<Cliente> clientes);
+        ClienteDto MapperEntityToDto(Cliente cliente);
+    }
+}
---
+++ RestApiModelDDD.Infrastructure/CrossCutting/Interfaces/IMapperProduto.cs
@@ -0,0 +1,17 @@
+﻿using RestApiModelDDD.Application.Dtos;
+using RestApiModelDDD.Domain.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApiModelDDD.Infrastructure.CrossCutting.Interfaces
+{
+    public interface IMapperProduto
+    {
+        Produto MapperDtoToEntity(ProdutoDto produtoDto);
+
+        IEnumerable<ProdutoDto> MapperListProdutosDto(IEnumerable<Produto> produtos);
+
+        ProdutoDto MapperEntityToDto(Produto produto);
+    }
+}
