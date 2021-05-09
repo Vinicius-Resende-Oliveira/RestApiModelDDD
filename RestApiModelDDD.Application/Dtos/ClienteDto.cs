d7b0b5aff538e0cc1206c157741e1697c1533025
Vinícius Oliveira <vinicius_rr1@hotmail.com>
2021-05-09 12:20:28 -0300

    Criando os Data Transfer Object(Dto)

---
+++ RestApiModelDDD.Application/Dtos/ClienteDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RestApiModelDDD.Application.Dtos
+{
+    public class ClienteDto
+    {
+        public int? Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+
+    }
+}
